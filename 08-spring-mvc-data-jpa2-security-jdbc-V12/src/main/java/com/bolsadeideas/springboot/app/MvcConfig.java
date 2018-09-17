a3777d60eefc45e7837ca275241e063cb61e67ef
Carlos Villanueva <ceva_19@hotmail.com>
2018-09-17 13:11:29 -0300

    Spring Boot MVC Data - Adding Security with BCrypt and JDBC

--- 08-spring-mvc-data-jpa2-security-jdbc-V12/src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
+++ 08-spring-mvc-data-jpa2-security-jdbc-V12/src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -1,6 +1,8 @@
 package com.bolsadeideas.springboot.app;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -34,5 +36,10 @@ public class MvcConfig implements WebMvcConfigurer {
 		registry.addViewController("/error_403").setViewName("error_403");;
 	}
 	
-	
+	//Registramos el bean BCrypt en el contenedor de Spring para luego ser inyectado
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder()
+	{
+		return new BCryptPasswordEncoder();
+	}
 }
--- 08-spring-mvc-data-jpa2-security-jdbc-V12/src/main/java/com/bolsadeideas/springboot/app/SpringBootDataJpaApplication.java
+++ 08-spring-mvc-data-jpa2-security-jdbc-V12/src/main/java/com/bolsadeideas/springboot/app/SpringBootDataJpaApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.bolsadeideas.springboot.app.models.service.IUploadService;
 
@@ -11,7 +12,10 @@ import com.bolsadeideas.springboot.app.models.service.IUploadService;
 public class SpringBootDataJpaApplication implements CommandLineRunner {
 
 	@Autowired
-	IUploadService uploadFileService;
+	private IUploadService uploadFileService;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDataJpaApplication.class, args);
@@ -25,5 +29,15 @@ public class SpringBootDataJpaApplication implements CommandLineRunner {
 		
 		uploadFileService.deleteAll();
 		uploadFileService.init();
+		
+		String password = "12345";
+		//loop que nos genera 2 password uno para andres y otra para admin
+		for(int i = 0; i < 2; i++)
+		{
+			String bcryptPassword = passwordEncoder.encode(password);
+			System.out.println(bcryptPassword);
+		}
+		//password 1: $2a$10$QqBtgiD9fign1RqyBWiwWeZmTnTzyFOdhCsB1t2JAr.Dl3wKF.RJe
+		//password 2: $2a$10$.oIuZ5XezUVv9gmsRTnZAOzdxMWBrhQQuVE.RY6ZuwcBIkWGJyl5i
 	}
 }
