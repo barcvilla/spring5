9cd6b40e7a7785d7c6e24b7a765b5556ec0565d5
Carlos Villanueva <ceva_19@hotmail.com>
2018-09-17 20:58:40 -0300

    Spring Boot MVC Data - Adding Spring Security with JPA Entities User and Authorities

---
+++ 09-spring-mvc-data-security-with-JPA-V13/src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
@@ -0,0 +1,73 @@
+package com.bolsadeideas.springboot.app;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.bolsadeideas.springboot.app.auth.handler.LoginSuccessHandler;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private LoginSuccessHandler successHandler;
+	
+	//Inyectamos el DataSource para la conexion con la Base de datos
+	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	//Implementamos un metodo para configurar el acceso a nuestras paginas y recursos
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		//configuramos nuestras rutas publicas de acceso a todos
+		http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/images/**", "/listar").permitAll()
+		//.antMatchers("/ver/**").hasAnyRole("USER")
+		//.antMatchers("/uploads/**").hasAnyRole("USER")
+		//.antMatchers("/form/**").hasAnyRole("ADMIN")
+		//.antMatchers("/eliminar/**").hasAnyRole("ADMIN")
+		//.antMatchers("/factura/**").hasAnyRole("ADMIN")
+		.anyRequest().authenticated()
+		.and()
+			.formLogin()
+				.successHandler(successHandler)
+				.loginPage("/login")
+			.permitAll()
+		.and()
+			.logout().permitAll()
+		.and()
+		.exceptionHandling().accessDeniedPage("/error_403"); //recursos privados
+	}
+	
+	//Implementamos el metodo que nos permite crear usuarios en memoria
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder build) throws Exception
+	{
+		build.jdbcAuthentication()
+		.dataSource(dataSource)
+		.passwordEncoder(passwordEncoder)
+		.usersByUsernameQuery("select username, password, enabled from users where username = ?")
+		.authoritiesByUsernameQuery("select u.username, a.authority from authorities a inner join users u on (a.user_id = u.id) where u.username= ?");
+		/**
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		//a traves del builder configuramos el repositorio donde guardamos los usuarios
+		UserBuilder users =  User.builder().passwordEncoder(encoder::encode);
+		
+		// creacion de nuestros usuarios en un repositorio en memoria
+		build.inMemoryAuthentication()
+		.withUser(users.username("admin").password("12345").roles("ADMIN","USER"))
+		.withUser(users.username("andres").password("12345").roles("USER"));
+		**/
+	}	
+}
