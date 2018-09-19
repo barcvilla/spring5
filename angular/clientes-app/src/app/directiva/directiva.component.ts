1e0a4cc3a5ab5977d85ab027a26a223f7f02b40f
Carlos Villanueva <ceva_19@hotmail.com>
2018-09-18 21:19:12 -0300

    Building Angular App with Spring 5

--- angular/clientes-app/src/app/directiva/directiva.component.ts
+++ angular/clientes-app/src/app/directiva/directiva.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-directiva',
-  templateUrl: './directiva.component.html',
-  styleUrls: ['./directiva.component.css']
+  templateUrl: './directiva.component.html'
 })
-export class DirectivaComponent implements OnInit {
+export class DirectivaComponent {
+  listaCurso:string[] = ['TypeScript', 'JavaScript', 'Java SE', 'C#', 'PHP'];
+  habilitar:boolean = true;
 
   constructor() { }
 
-  ngOnInit() {
+  setHabilitar(): void
+  {
+    this.habilitar = (this.habilitar==true) ? false : true;
   }
-
 }
